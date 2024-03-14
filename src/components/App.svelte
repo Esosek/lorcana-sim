c9ee261b94c08d9e14c7ff69b778d2dc556263f0
Ales Zima <ales@cpex.cz>
2024-03-14 13:56:01 +0100

    Added basic styling

---
+++ src/components/App.svelte
@@ -0,0 +1,10 @@
+<script>
+    let arePacksSelected = true;
+
+    function toggleSelected() {
+        arePacksSelected = !arePacksSelected;
+    }
+</script>
+
+{#if arePacksSelected}<h1>Selected</h1>{:else}<h1>Not selected</h1>{/if}
+<button on:click={toggleSelected}>Swap</button>
