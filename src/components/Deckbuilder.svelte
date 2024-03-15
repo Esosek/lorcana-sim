22df6eefbcdbaa5aa73053c4d885c4d42e9000eb
Ales Zima <ales@cpex.cz>
2024-03-15 15:03:49 +0100

    Added card rendering in Deckbuilder

--- src/components/Deckbuilder.svelte
+++ src/components/Deckbuilder.svelte
@@ -1,1 +1,37 @@
-<h1>You are here</h1>
+<script>
+  import { cardPool } from '../stores/cardPool';
+</script>
+
+<main>
+  <ul class="flex-group">
+    {#each $cardPool as card}
+      <li>
+        <img
+          src={card.images.thumbnail}
+          alt={`Image of ${card.fullName} card`}
+          width="250"
+          height="349"
+        />
+      </li>
+    {/each}
+  </ul>
+</main>
+
+<style>
+  .flex-group {
+    align-items: start;
+    gap: 1.5rem;
+  }
+
+  li {
+    max-width: 250px;
+    border-radius: 0.5rem;
+    overflow: hidden;
+    transition: transform 150ms ease-out;
+  }
+
+  li:hover {
+    cursor: pointer;
+    transform: scale(1.025);
+  }
+</style>
