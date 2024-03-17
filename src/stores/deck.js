d344456c8ff378c64e25f33d8c969ec6f486e6b0
Ales Zima <ales@cpex.cz>
2024-03-17 13:50:42 +0100

    Added deck mockup

---
+++ src/stores/deck.js
@@ -0,0 +1,29 @@
+import { writable, get } from 'svelte/store';
+const deckMockup = [
+  {
+    color: 'Sapphire',
+    cost: 4,
+    fullName: 'Vault Door',
+    inkwell: true,
+    id: 603,
+  },
+  {
+    color: 'Steel',
+    cost: 4,
+    fullName: 'And Then Along Came Zeus',
+    inkwell: false,
+    id: 631,
+  },
+];
+
+const deck = writable(deckMockup);
+
+function addCard(card) {}
+
+function removeCard(card) {}
+
+export default {
+  subscribe: deck.subscribe,
+  add: addCard,
+  remove: removeCard,
+};
