1db4cdd6209935682460d33cedc1cfb1fd97891a
Ales Zima <ales@cpex.cz>
2024-03-18 08:18:14 +0100

    Outsourced deckMockup

---
+++ src/data/deckMockup.js
@@ -0,0 +1,30 @@
+export default deckMockup = [
+  {
+    color: 'Sapphire',
+    cost: 4,
+    baseName: 'Vault Door',
+    inkwell: true,
+    quantity: 1,
+    type: 'Item',
+    id: 603,
+  },
+  {
+    color: 'Steel',
+    cost: 4,
+    baseName: 'And Then Along Came Zeus',
+    inkwell: false,
+    quantity: 2,
+    type: 'Action',
+    id: 631,
+  },
+  {
+    color: 'Amber',
+    cost: 10,
+    baseName: 'Chernabog',
+    subtitle: 'Evildoer',
+    inkwell: false,
+    quantity: 1,
+    type: 'Character',
+    id: 435,
+  },
+];
--- src/stores/deck.js
+++ src/stores/deck.js
@@ -1,36 +1,6 @@
 import { writable, get } from 'svelte/store';
-const deckMockup = [
-  {
-    color: 'Sapphire',
-    cost: 4,
-    baseName: 'Vault Door',
-    inkwell: true,
-    quantity: 1,
-    type: 'Item',
-    id: 603,
-  },
-  {
-    color: 'Steel',
-    cost: 4,
-    baseName: 'And Then Along Came Zeus',
-    inkwell: false,
-    quantity: 2,
-    type: 'Action',
-    id: 631,
-  },
-  {
-    color: 'Amber',
-    cost: 10,
-    baseName: 'Chernabog',
-    subtitle: 'Evildoer',
-    inkwell: false,
-    quantity: 1,
-    type: 'Character',
-    id: 435,
-  },
-];
 
-const deck = writable(deckMockup);
+const deck = writable([]);
 
 function addCard(card) {}
 
