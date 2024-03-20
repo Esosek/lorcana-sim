cc6901050f9eacf936eb60e4a5c59cba14f04de1
Ales Zima <ales@cpex.cz>
2024-03-20 14:20:17 +0100

    Added Netlify integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import svelte from "@astrojs/svelte";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-  integrations: [svelte()]
+  integrations: [svelte()],
+  adapter: netlify()
 });
