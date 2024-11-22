aee6d07ef2610f643c8c49d68bb13cc075474eaf
Ales Zima <ales@cpex.cz>
2024-11-22 09:55:56 +0100

    Added Azurite Sea cards

--- src/stores/options.js
+++ src/stores/options.js
@@ -1,15 +1,15 @@
-import { writable } from 'svelte/store';
+import { writable } from 'svelte/store'
 
 export const GameMode = {
   Sealed: 'Sealed',
   Draft: 'Draft',
-};
+}
 
 export function pickGameMode(gameMode) {
   options.update((prevOptions) => {
-    prevOptions.gameMode = gameMode;
-    return { ...prevOptions };
-  });
+    prevOptions.gameMode = gameMode
+    return { ...prevOptions }
+  })
 }
 
 // Numeric table for sorting purposes
@@ -20,20 +20,20 @@ export const RarityTable = {
   Rare: 3,
   Uncommon: 4,
   Common: 5,
-};
+}
 
 export const SortOptions = {
   Ink: 'Ink',
   Rarity: 'Rarity',
-};
+}
 
 const defaultOptions = {
   sortBy: SortOptions.Ink,
   ascending: true,
   isBuilding: false,
   gameMode: GameMode.Sealed,
-};
+}
 
-const options = writable(defaultOptions);
+const options = writable(defaultOptions)
 
-export default options;
+export default options
